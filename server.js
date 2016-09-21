9ef3f7e19660f9c4033722572309cfe7039979ba
jithijose <jithijose@gmail.com>
2016-09-21 22:46:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleName = article-one
     var articleName = req.params.articleName;
-  res.send(createTemplate(article[articleName]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
