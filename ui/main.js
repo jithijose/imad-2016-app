e10439368308a8f2dc8419aefff39ba8b7b6eae1
jithijose <jithijose@gmail.com>
2016-09-24 09:41:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,35 @@ button.onclick = function(){
   request.send(null);
 
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('btn_submit');
+sumbit.onClick = function(){
+    //Make the request to server and send name
+    
+    //Capture the list of name and render as HTML
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0; i < names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
