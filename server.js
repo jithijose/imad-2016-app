9249a023668cf9fe45b0548480eb713acce7481c
jithijose <jithijose@gmail.com>
2016-09-24 11:05:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,9 +109,10 @@ app.get('/counter',function(req, res){
 });
 
 var names = [];
-app.get('/submit-name/:name',function(req, res){
+app.get('/submit-name',function(req, res){
+    //query paramater /submit-name?name=xxxx
    //Get the name from the request
-   var name = req.params.name;
+   var name = req.query.name;
    
    names.push(name);
    res.send(JSON.stringify(names));
